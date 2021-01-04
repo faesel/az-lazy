c78feaf458ca0106ec9086e83642bf16c76df4dc
Faesel Saeed <faesel.saeed@plumguide.com>
2021-01-04 09:08:30 +0000

    updated remove and select connection

--- az-lazy/Commands/Connection/Executor/RemoveConnectionExecutor.cs
+++ az-lazy/Commands/Connection/Executor/RemoveConnectionExecutor.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Threading.Tasks;
 using az_lazy.Helpers;
 using az_lazy.Manager;
+using Spectre.Console;
 
 namespace az_lazy.Commands.Connection.Executor
 {
@@ -18,21 +20,35 @@ namespace az_lazy.Commands.Connection.Executor
         {
             if (!string.IsNullOrEmpty(opts.RemoveConnection))
             {
-                var removeMessage = $"Removing connection {opts.RemoveConnection}";
-                ConsoleHelper.WriteInfoWaiting(removeMessage, true);
+                AnsiConsole
+                    .Status()
+                    .Spinner(Spinner.Known.Star)
+                    .SpinnerStyle(Style.Parse("green bold"))
+                    .StartAsync($"Removing connection {opts.RemoveConnection} ...", _ =>
+                    {
+                        try
+                        {
+                            var isSuccessfull = LocalStorageManager.RemoveConnection(opts.RemoveConnection);
 
-                var isSuccessfull = LocalStorageManager.RemoveConnection(opts.RemoveConnection);
+                            if (isSuccessfull)
+                            {
+                                AnsiConsole.MarkupLine($"Removing connection {opts.RemoveConnection} ... [bold green]Successful[/]");
+                                AnsiConsole.MarkupLine($"Finished removing connection {opts.RemoveConnection}");
+                            }
+                            else
+                            {
+                                AnsiConsole.MarkupLine($"Removing connection {opts.RemoveConnection} ... [bold red]Failed[/]");
+                                ConsoleHelper.WriteLineError("Check the connection name exists and try again");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            AnsiConsole.MarkupLine($"Error whilst removing connection {opts.RemoveConnection} ... [bold red]Failed[/]");
+                            AnsiConsole.MarkupLine($"[bold red]{ex.Message}[/]");
+                        }
 
-                if(isSuccessfull)
-                {
-                    ConsoleHelper.WriteLineSuccessWaiting(removeMessage);
-                    ConsoleHelper.WriteLineNormal($"Finished removing connection {opts.RemoveConnection}");
-                }
-                else
-                {
-                    ConsoleHelper.WriteLineFailedWaiting(removeMessage);
-                    ConsoleHelper.WriteLineError("Check the connection name exists and try again");
-                }
+                        return Task.CompletedTask;
+                    });
             }
 
             return Task.CompletedTask;
