788d2c8c585d8df6e2dcb538fb423051ccb7d0d3
Faesel Saeed <faesel.saeed@plumguide.com>
2021-02-15 08:39:53 +0000

    updated sample code and added code to escape values

--- az-lazy/Commands/Table/Executor/SampleExecutor.cs
+++ az-lazy/Commands/Table/Executor/SampleExecutor.cs
@@ -25,55 +25,63 @@ namespace az_lazy.Commands.Table.Executor
         {
             if(!string.IsNullOrEmpty(opts.Sample))
             {
-                var infoMessage = $"Sampleing table {opts.Sample}";
-                ConsoleHelper.WriteInfoWaiting(infoMessage, true);
-
-                try
-                {
-                    var selectedConnection = LocalStorageManager.GetSelectedConnection();
-                    var sampledEntities = await AzureTableManager.Sample(selectedConnection.ConnectionString, opts.Sample, opts.SampleCount);
-
-                    ConsoleHelper.WriteLineSuccessWaiting(infoMessage);
-
-                    if(sampledEntities == null || sampledEntities.Count == 0)
+                await AnsiConsole
+                    .Status()
+                    .Spinner(Spinner.Known.Star)
+                    .SpinnerStyle(Style.Parse("green bold"))
+                    .StartAsync($"Sampling table {opts.Sample} ... ", async _ =>
                     {
-                        ConsoleHelper.WriteInfo("No rows found");
-                    }
+                        try
+                        {
+                            var selectedConnection = LocalStorageManager.GetSelectedConnection();
+                            var sampledEntities = await AzureTableManager.Sample(selectedConnection.ConnectionString, opts.Sample, opts.SampleCount);
 
-                    var table = new Spectre.Console.Table();
-                    table.Border(TableBorder.DoubleEdge);
-                    table.Expand();
-                    table.LeftAligned();
+                            
 
-                    table.AddColumn("[yellow]Number[/]");
-                    table.AddColumn("[yellow]Partition Key[/]");
-                    table.AddColumn("[yellow]Row Key[/]");
+                            if(sampledEntities == null || sampledEntities.Count == 0)
+                            {
+                                AnsiConsole.WriteLine("No rows found");
+                            }
+                            else
+                            {
+                                AnsiConsole.MarkupLine($"Sampling table {opts.Sample} ... [bold green]Successful[/]");
 
-                    foreach(var column in sampledEntities[0].Properties)
-                    {
-                        table.AddColumn($"[yellow]{column.Key}[/]");
-                    }
-                    table.AddColumn("[yellow]Timestamp[/]");
+                                var table = new Spectre.Console.Table();
+                                table.Border(TableBorder.DoubleEdge);
+                                table.Expand();
+                                table.LeftAligned();
 
-                    foreach(var row in sampledEntities)
-                    {
-                        var values = new List<Markup>();
-                        values.Add(new Markup($"[grey62]{sampledEntities.IndexOf(row) + 1}[/]"));
-                        values.Add(new Markup($"[grey93]{row.PartitionKey}[/]"));
-                        values.Add(new Markup($"[grey93]{row.RowKey}[/]"));
-                        values.AddRange(row.Properties.Select(x => new Markup($"[grey62]{x.Value}[/]")).ToList());
-                        values.Add(new Markup($"[grey62]{row.Timestamp}[/]"));
+                                table.AddColumn("[yellow]Number[/]");
+                                table.AddColumn("[yellow]Partition Key[/]");
+                                table.AddColumn("[yellow]Row Key[/]");
 
-                        table.AddRow(values.ToArray());
-                    }
-                    
-                    AnsiConsole.Render(table);
-                }
-                catch (Exception ex)
-                {
-                    ConsoleHelper.WriteLineError(ex.Message);
-                    ConsoleHelper.WriteLineFailedWaiting($"Failed to sample table {opts.Sample}");
-                }
+                                foreach(var column in sampledEntities[0].Properties)
+                                {
+                                    table.AddColumn($"[yellow]{column.Key}[/]");
+                                }
+                                table.AddColumn("[yellow]Timestamp[/]");
+
+                                foreach(var row in sampledEntities)
+                                {
+                                    var values = new List<Markup>();
+                                    values.Add(new Markup($"[grey62]{sampledEntities.IndexOf(row) + 1}[/]"));
+                                    values.Add(new Markup($"[grey93]{row.PartitionKey}[/]"));
+                                    values.Add(new Markup($"[grey93]{row.RowKey}[/]"));
+                                    values.AddRange(row.Properties.Select(x => new Markup($"[grey62]{Markup.Escape(x.Value.ToString())}[/]")).ToList());
+                                    values.Add(new Markup($"[grey62]{row.Timestamp}[/]"));
+
+                                    table.AddRow(values.ToArray());
+                                }
+                                
+                                AnsiConsole.Render(table);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            AnsiConsole.MarkupLine($"Sampling table {opts.Sample} ... [bold red]Failed[/]");
+                            AnsiConsole.MarkupLine($"[bold red]{ex.Message}[/]");
+                        }
+                    });
             }
         }
     }
